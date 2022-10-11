3b30ed194dc1b66c9ff2eaee1563462e1a214e46
Ian <istreator@yahoo.com>
2022-10-11 14:44:36 -0500

    connected to mongoDB, created userSchema and user creation functionality, using bcrypt for password hashing.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log("server...")
+const dotenv = require("dotenv").config();
+const express = require("express");
+const colors = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(201).json({ message: "welcome to our ticket system api." });
+});
+
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`server listening on port: ${PORT}`);
+});
